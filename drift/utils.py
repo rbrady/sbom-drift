53f8ac5d03dc212252b4f43fad376f447ddc7319
Toure Dunnon <toure.dunnon@anchore.com>
2021-09-28 17:42:41 -0400

    Added filter functions to process SBOM datastructure.
    Signed-off-by: Toure Dunnon <toure.dunnon@anchore.com>

--- drift/utils.py
+++ drift/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pydantic import parse_file_as
+from typing import List
 
 from .data.model import SBOM
 
@@ -21,3 +22,41 @@ def load_sbom_from_json(input_data: dict) -> SBOM:
     :return: SBOM object
     """
     return SBOM(**input_data)
+
+
+def filter_sbom(filter_list: set, sbom_data: SBOM): -> List[dict]:
+    """Filter SBOM by a set of keywords
+
+    >  filter_sbom({'name','type'}, ot)
+    >  [{'artifacts': [{'name': 'SQLAlchemy', 'type': 'python'}]},
+        {'artifacts': [{'name': 'fastapi', 'type': 'python'}]},
+        {'artifacts': [{'name': 'uvicorn', 'type': 'python'}]}]
+
+    """
+    sbom_filter = []
+
+    for index, _ in enumerate(sbom_data.artifacts):
+        # TODO: artifacts keyword could be passed in as a parameter to the
+        # function.
+        sbom_filter.append(sbom_data.dict(include={'artifacts': {index: filter_list}}))
+
+    return sbom_filter
+
+
+def convert_filter_to_sets(filtered_sbom: List[dict]) -> List[set]:
+    """Convert the filtered sbom into a list of sets
+    > convert_filter_to_sets(< data structure here>)
+    > [{('name', 'SQLAlchemy'), ('type', 'python')},
+       {('name', 'fastapi'), ('type', 'python')},
+       {('name', 'uvicorn'), ('type', 'python')}]
+
+    """
+    converted_list_set = []
+
+    for data in filtered_sbom:
+        # TODO: artifacts keyword could be passed in as a parameter along
+        # index.
+        converted_list_set.append(set(data['artifacts'][0].items()))
+
+    return converted_list_set
+
